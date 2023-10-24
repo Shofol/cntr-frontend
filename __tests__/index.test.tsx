871594ebcb6664ed80598935686cbb5e369d228f
anowerjahan <anower@mycontourhealth.com>
2023-10-25 00:54:03 +0600

    test link

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,9 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+// import { useRouter } from "next/router";
 import Home from "~/pages";
 
+const useRouter = jest.spyOn(require("next/router"), "useRouter");
+
 describe("Home", () => {
+  const router = { push: jest.fn() };
+  useRouter.mockReturnValue(router);
+
   it("renders a heading", () => {
     render(<Home />);
 
@@ -11,6 +18,11 @@ describe("Home", () => {
       level: 1,
       name: /Care you can count on/i,
     });
+
+    const signInButton: HTMLButtonElement = screen.getByText("Log in");
+    expect(signInButton).toBeInTheDocument();
+    fireEvent.click(signInButton);
+    expect(router.push).toHaveBeenCalledWith("/auth/signin");
 
     expect(heading).toBeInTheDocument();
   });
